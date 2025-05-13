f01f2e9a8da763c6296f8a525372b7519393c041
viniwittler <105309416+viniwittler@users.noreply.github.com>
2025-05-13 09:38:06 -0300

    fix: ajust caso de teste partição de equivalencia

--- beecrowd/src/test/java/com/b3306/CalcularTest.java
+++ beecrowd/src/test/java/com/b3306/CalcularTest.java
@@ -5,24 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CalcularTest {
 
-    @Test
-    public void testeParticao() {
-        int[] vetor = {0, 2, 4, 6}; 
-        Calcular calcula = new Calcular(vetor);
-
-        calcula.adicionar(1, 2, 2); 
-        assertEquals(2, calcula.maximoDivisorComum(1, 3));
-    }
-
-    @Test
-    public void testeValorLimite() {
-	// limite = 1
-        int[] vetor = {0, 7}; 
-        Calcular calcula = new Calcular(vetor);
-
-        assertEquals(7, calcula.maximoDivisorComum(1, 1));
-        calcula.adicionar(1, 1, 3); 
-        assertEquals(10, calcula.maximoDivisorComum(1, 1));
-    }
+	//Partição de equivalencia
+	@Test
+	public void testeMDC_MesmoDivisor() {
+	    int[] entrada = {4, 6, 8};
+	    Calcular calcular = new Calcular(entrada);
+	    assertEquals(2, calcular.maximoDivisorComum(0, 2));
+	}
+	
+	@Test
+	public void testeMDC_ValoresSemDivisor() {
+	    int[] entrada = {5, 9, 14};
+	    Calcular calcular = new Calcular(entrada);
+	    assertEquals(1, calcular.maximoDivisorComum(0, 2));
+	}
+	
+	@Test
+	public void testeMDC_Iguais() {
+	    int[] entrada = {6, 6, 6};
+	    Calcular calcular = new Calcular(entrada);
+	    assertEquals(6, calcular.maximoDivisorComum(0, 2));
+	}
 
 }
